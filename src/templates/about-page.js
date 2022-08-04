5097650a526782d14ed997f020579d1ac27f12f2
Tarun kumar <61449919+tarunkumar519@users.noreply.github.com>
2022-08-04 17:49:50 +0530

    Update

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -27,6 +27,30 @@ const AboutPage = ({ data }) => {
         <h1>{frontmatter.title}</h1>
         <article dangerouslySetInnerHTML={{ __html: html }} />
         <h1>PCWRA Members</h1>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754121155645500/lingarao.jpg" class="profile" />
+  <figcaption>
+      <h2>Linga Rao</h2>
+      <h2>Boyanapally</h2>
+    <h4>President</h4>
+  </figcaption>
+</figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754120971108372/janardhan.jpg" class="profile" />
+  <figcaption>
+    <h2>Bhandari</h2>
+      <h2>Janardhan Goud</h2>
+    <h4>Working President/General Secretary</h4>
+  </figcaption>
+</figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754120174190644/sampath.jpg" class="profile" />
+  <figcaption>
+      <h2>Sampath Kumar</h2>
+      <h2>Neelam</h2>
+    <h4>Treasurer</h4>
+  </figcaption>
+</figure>
     <figure class="snip1390">
   <img src="https://cdn.discordapp.com/attachments/914907771063894059/1004651664915042395/IMG_0634_1.jpg" class="profile" />
   <figcaption>
@@ -43,6 +67,38 @@ const AboutPage = ({ data }) => {
     <h4>Joint Secretary</h4>
   </figcaption>
 </figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754119884771358/prasad.jpg" class="profile" />
+  <figcaption>
+    <h2>Prasad Madadi</h2>
+    <h4>Vice President</h4>
+  </figcaption>
+</figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754120732024952/ramesh.jpg" class="profile" />
+  <figcaption>
+    <h2>Ramesh</h2>
+      <h2>Kodumuri</h2>
+    <h4>Vice President</h4>
+  </figcaption>
+</figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754118731341864/tarun.jpeg" class="profile" />
+  <figcaption>
+    <h2>Tarun Reddy</h2>
+      <h2>Chintakuntla</h2>
+    <h4>Executive Member</h4>
+  </figcaption>
+</figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1001754120446816298/pasha.jpg" class="profile" />
+  <figcaption>
+    <h2>Faqeer Pasha</h2>
+      <h2>Mohammed</h2>
+    <h4>Executive Member</h4>
+  </figcaption>
+</figure>
+
       </div>
     </Layout>
   )
