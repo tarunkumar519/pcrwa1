0ea0ab7f9830ae623cd4b03ee024fb6be3c15457
Tarun kumar <61449919+tarunkumar519@users.noreply.github.com>
2022-08-04 17:29:49 +0530

    Update

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -26,12 +26,21 @@ const AboutPage = ({ data }) => {
       <div className="wrapper">
         <h1>{frontmatter.title}</h1>
         <article dangerouslySetInnerHTML={{ __html: html }} />
+        <h1>PCWRA Members</h1>
     <figure class="snip1390">
   <img src="https://cdn.discordapp.com/attachments/914907771063894059/1004651664915042395/IMG_0634_1.jpg" class="profile" />
   <figcaption>
     <h2>Aennamaneni</h2>
       <h2>Ram Gopal Rao</h2>
     <h4>Chief Advisor</h4>
+  </figcaption>
+</figure>
+        <figure class="snip1390">
+  <img src="https://cdn.discordapp.com/attachments/914907771063894059/1004720156657852496/168C9966-7CD9-4B6C-A206-0C0797F52225.jpeg" class="profile" />
+  <figcaption>
+    <h2>Mohammed</h2>
+      <h2>Abdul Waseem</h2>
+    <h4>Joint Secretary</h4>
   </figcaption>
 </figure>
       </div>
