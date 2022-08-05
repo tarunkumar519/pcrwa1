f3653d61f7e2ac0577eb270cd771f9d3fbf0e64b
Tarun kumar <tarunreddy17519@gmail.com>
2022-08-05 19:45:21 +0530

    1

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -38,91 +38,91 @@ const AboutPage = ({ data }) => {
         <article dangerouslySetInnerHTML={{ __html: html }} />
         <h1>PCRWA Members</h1>
         <figure class="snip1390">
-  <img src="{Lingarao}" class="profile" />
-  <figcaption>
-      <h2>Linga Rao</h2>
-      <h2>Boyanapally</h2>
-    <h4>President</h4>
-  </figcaption>
-</figure>
+		<img src="{Lingarao}" class="profile" />
+		<figcaption>
+		<h2>Linga Rao</h2>
+		<h2>Boyanapally</h2>
+		<h4>President</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Janardhan}" class="profile" />
-  <figcaption>
-    <h2>Bhandari</h2>
-      <h2>Janardhan Goud</h2>
-    <h4>Working President/General Secretary</h4>
-  </figcaption>
-</figure>
+		<img src="{Janardhan}" class="profile" />
+		<figcaption>
+		<h2>Bhandari</h2>
+		<h2>Janardhan Goud</h2>
+		<h4>Working President/General Secretary</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Sampathkumar}" class="profile" />
-  <figcaption>
-      <h2>Sampath Kumar</h2>
-      <h2>Neelam</h2>
-    <h4>Treasurer</h4>
-  </figcaption>
-</figure>
-    <figure class="snip1390">
-  <img src="{Ramgopalrao}" class="profile" />
-  <figcaption>
-    <h2>Aennamaneni</h2>
-      <h2>Ram Gopal Rao</h2>
-    <h4>Chief Advisor</h4>
-  </figcaption>
-</figure>
+		<img src="{Sampathkumar}" class="profile" />
+		<figcaption>
+		<h2>Sampath Kumar</h2>
+		<h2>Neelam</h2>
+		<h4>Treasurer</h4>
+		</figcaption>
+		</figure>
+		<figure class="snip1390">
+		<img src="{Ramgopalrao}" class="profile" />
+		<figcaption>
+		<h2>Aennamaneni</h2>
+		<h2>Ram Gopal Rao</h2>
+		<h4>Chief Advisor</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{rameshgoud.png}" class="profile" />
-  <figcaption>
-    <h2>Thallapally</h2>
-    <h2>Ramesh Goud</h2>
-    <h4>Legal Advisor</h4>
-  </figcaption>
-</figure>
+		<img src="{rameshgoud.png}" class="profile" />
+		<figcaption>
+		<h2>Thallapally</h2>
+		<h2>Ramesh Goud</h2>
+		<h4>Legal Advisor</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Prasad}" class="profile" />
-  <figcaption>
-    <h2>Prasad Madadi</h2>
-    <h4>Joint Secretary</h4>
-  </figcaption>
-</figure>
+		<img src="{Prasad}" class="profile" />
+		<figcaption>
+		<h2>Prasad Madadi</h2>
+		<h4>Joint Secretary</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Subbarao}" class="profile" />
-  <figcaption>
-    <h2>Subbarao Gonuguntla</h2>
-    <h4></h4>
-  </figcaption>
-</figure
+		<img src="{Subbarao}" class="profile" />
+		<figcaption>
+		<h2>Subbarao Gonuguntla</h2>
+		<h4></h4>
+		</figcaption>
+		</figure
         <figure class="snip1390">
-  <img src="{Waseem}" class="profile" />
-  <figcaption>
-    <h2>Mohammed</h2>
-      <h2>Abdul Waseem</h2>
-    <h4>Joint Secretary</h4>
-  </figcaption>
-</figure>
+		<img src="{Waseem}" class="profile" />
+		<figcaption>
+		<h2>Mohammed</h2>
+		<h2>Abdul Waseem</h2>
+		<h4>Joint Secretary</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Rameshkodumuri}" class="profile" />
-  <figcaption>
-    <h2>Ramesh</h2>
-      <h2>Kodumuri</h2>
-    <h4>Vice President</h4>
-  </figcaption>
-</figure>
+		<img src="{Rameshkodumuri}" class="profile" />
+		<figcaption>
+		<h2>Ramesh</h2>
+		<h2>Kodumuri</h2>
+		<h4>Vice President</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Tarun}" class="profile" />
-  <figcaption>
-    <h2>Tarun Reddy</h2>
-      <h2>Chintakuntla</h2>
-    <h4>Executive Member</h4>
-  </figcaption>
-</figure>
+		<img src="{Tarun}" class="profile" />
+		<figcaption>
+		<h2>Tarun Reddy</h2>
+		<h2>Chintakuntla</h2>
+		<h4>Executive Member</h4>
+		</figcaption>
+		</figure>
         <figure class="snip1390">
-  <img src="{Pasha}" class="profile" />
-  <figcaption>
-    <h2>Faqeer Pasha</h2>
-      <h2>Mohammed</h2>
-    <h4>Executive Member</h4>
-  </figcaption>
-</figure>
+		<img src="{Pasha}" class="profile" />
+		<figcaption>
+		<h2>Faqeer Pasha</h2>
+		<h2>Mohammed</h2>
+		<h4>Executive Member</h4>
+		</figcaption>
+		</figure>
 
       </div>
     </Layout>
