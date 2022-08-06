ddda9d50a8c8eb8c974819002335cb316e0d820f
Tarun kumar <61449919+tarunkumar519@users.noreply.github.com>
2022-08-06 11:30:53 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -8,15 +8,6 @@ const Header = ({ children }) => (
       bg: "siteColor",
     }}
   >
-    <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2C90NNX5CG"></script>
-  <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2C90NNX5CG');
-  </script>
 
     {children}
   </header>
